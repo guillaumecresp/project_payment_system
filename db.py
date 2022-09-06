1d9f0a3337f11250204a9aa54ff9d2fbca0d044d
guillaume cresp <guillaume.cresp@epitech.eu>
2022-09-06 10:50:26 -0400

    set_db_completed

--- db.py
+++ db.py
@@ -1,7 +1,7 @@
 import sqlite3
 con = sqlite3.connect("tutorial.db")
 cur = con.cursor()
-cur.execute("CREATE TABLE test(User, age, inscription_date)")
+cur.execute("CREATE TABLE test(User_id, lastname, firstname, service, amount)")
 res = cur.execute("SELECT name FROM sqlite_master")
 print(res.fetchone())
 res = cur.execute("SELECT name FROM sqlite_master")
@@ -10,38 +10,16 @@ res = cur.execute("SELECT name FROM sqlite_master WHERE name='spam'")
 print(res.fetchone() is None)
 cur.execute("""
     INSERT INTO test VALUES
-        ('Gerard Alvès', 45, '01-01-2014'),
-        ('Cristiano Ronaldo', 25, '01-01-2014')
+        ('1', "poutine", 'vald', 'reparer isolation', '50€'),
+        ('2', "poutine", 'kashimir', 'reparer gaz', '130€')
 """)
 con.commit()
-res = cur.execute("SELECT inscription_date FROM test")
+res = cur.execute("SELECT amount FROM test")
 print(res.fetchall())
 data = [
-    ("Monty Burns", 54, '7-01-2014'),
-    ("Homer Simpson", 35, '7-02-2014'),
-    ("Dark Vador", 45, '7-03-2014'),
+    ("3", "simpson", 'homer', 'reparer maison', '10€'),
+    ("4", "simpson", 'lisa', 'reparer voiture', '20€'),
+    ("5", "simpson", 'bart', 'reparer ordinateur', '30€'),
 ]
-
-cur.execute("CREATE TABLE payment(payment_method, id, amount, date)")
-res = cur.execute("SELECT name FROM sqlite_master")
-print(res.fetchone())
-res = cur.execute("SELECT name FROM sqlite_master")
-print(res.fetchone())
-res = cur.execute("SELECT name FROM sqlite_master WHERE name='spam'")
-print(res.fetchone() is None)
-cur.execute("""
-    INSERT INTO payment VALUES
-        ('Monty Python and the Holy Grail', 1975, 8.2),
-        ('And Now for Something Completely Different', 1971, 7.5)
-""")
-con.commit()
-res = cur.execute("SELECT score FROM payment")
-print(res.fetchall())
-data = [
-    ("Monty Python Live at the Hollywood Bowl", 1982, 7.9),
-    ("Monty Python's The Meaning of Life", 1983, 7.5),
-    ("Monty Python's Life of Brian", 1979, 8.0),
-]
-cur.executemany("INSERT INTO test VALUES(?, ?, ?)", data)
-cur.executemany("INSERT INTO test VALUES(?, ?, ?)", data)
+cur.executemany("INSERT INTO test VALUES(?, ?, ?, ?, ?)", data)
 con.commit()
